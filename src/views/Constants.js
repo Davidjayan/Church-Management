3399bde4b385b84d358163b2db25cb6fb3f348e5
Davidjayan <davidjayandj@gmail.com>
2022-06-12 15:34:58 +0530

    [Davdidjayan] url changed

--- src/views/Constants.js
+++ src/views/Constants.js
@@ -1,2 +1,2 @@
 // export const url = "http://backendjnag.rf.gd";
-export const url = "http://localhost/Church-management-api/api"; 
+export const url = "https://admin-management-website.000webhostapp.com/Church-management-api/api"; 
